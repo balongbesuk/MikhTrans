bda1907f3ab75bc55c6e74c4cf7a42e531d72d7d
balongbesuk <desa.balongbesuk@gmail.com>
2026-06-28 18:55:52 +0700

    chore: remove unused config examples and utility script

--- replace.ps1
+++
@@ -1,8 +0,0 @@
-$utf8NoBom = New-Object System.Text.UTF8Encoding $false
-Get-ChildItem -Path . -Recurse -File | Where-Object { $_.DirectoryName -notmatch '\\.git' -and $_.Extension -notmatch '\.(png|jpg|jpeg|gif|ico|woff2?|ttf|eot|db|sqlite3?|pdf|zip|tar|gz|rar)' } | ForEach-Object {
-    $path = $_.FullName
-    $text = [System.IO.File]::ReadAllText($path)
-    if ($text.Contains('MikhPay')) {
-        [System.IO.File]::WriteAllText($path, $text.Replace('MikhPay', 'MikhPay'), $utf8NoBom)
-    }
-}
